e6b62830d0bee31483f75587e004d89795acc286
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-01-31 09:47:40 +0300

    Change that fixes toolbar error

--- app/src/main/java/com/example/anthonykiplimo/alc30/RegisterActivity.java
+++ app/src/main/java/com/example/anthonykiplimo/alc30/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.anthonykiplimo.alc30;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,9 +19,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private TextInputLayout mRegUsername;
-    private TextInputLayout mRegEmail;
-    private TextInputLayout mRegPassword;
+    private TextInputEditText mRegUsername;
+    private TextInputEditText mRegEmail;
+    private TextInputEditText mRegPassword;
     private Button mRegButton;
     private FirebaseAuth mAuth;
 
@@ -39,9 +40,9 @@ public class RegisterActivity extends AppCompatActivity {
         mRegButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mRegUsername.getEditText().getText().toString();
-                String email = mRegEmail.getEditText().getText().toString();
-                String password = mRegPassword.getEditText().getText().toString();
+                String username = mRegUsername.getEditableText().toString();
+                String email = mRegEmail.getEditableText().toString();
+                String password = mRegPassword.getEditableText().toString();
 
                 register_user(username, email, password);
             }
